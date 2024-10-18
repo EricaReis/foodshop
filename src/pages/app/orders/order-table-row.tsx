6c3f2c0b90012907eb992be07abd8481635ae02a
EricaReis <ericareis67@outlook.com>
2024-10-17 22:48:26 -0300

    Update orders table with new info

--- src/pages/app/orders/order-table-row.tsx
+++ src/pages/app/orders/order-table-row.tsx
@@ -1,19 +1,32 @@
+import { formatDistanceToNow } from "date-fns";
+import { ArrowRight, Search, X } from "lucide-react";
+
 import { Button } from "@/components/ui/button";
 import { Dialog, DialogTrigger } from "@/components/ui/dialog";
 import { TableCell, TableRow } from "@/components/ui/table";
-import { ArrowRight, Search, X } from "lucide-react";
-import { OrderDetails } from "./order-details";
 
-export function OrderTableRow() {
+import { OrderDetails } from "./order-details";
+import { OrderStatus } from "@/components/order-status";
+
+interface OrderTableRowProps {
+  order: {
+    orderId: string;
+    createdAt: string;
+    status: "pending" | "canceled" | "processing" | "delivering" | "delivered";
+    customerName: string;
+    total: number;
+  };
+}
+
+export function OrderTableRow({ order }: OrderTableRowProps) {
   return (
     <TableRow>
       <TableCell>
         <Dialog>
           <DialogTrigger asChild>
             <Button variant="outline" size="xs">
-              <Search className="h-3 w-3">
-                <span className="sr-only">Order details</span>
-              </Search>
+              <Search className="h-3 w-3" />
+              <span className="sr-only">Detalhes do pedido</span>
             </Button>
           </DialogTrigger>
 
@@ -21,27 +34,33 @@ export function OrderTableRow() {
         </Dialog>
       </TableCell>
       <TableCell className="font-mono text-xs font-medium">
-        23908fsdf349sad
+        {order.orderId}
       </TableCell>
-      <TableCell className="text-muted-foreground">15 minutes ago</TableCell>
+      <TableCell className="text-muted-foreground">
+        {formatDistanceToNow(order.createdAt, {
+          addSuffix: true,
+        })}
+      </TableCell>
       <TableCell>
-        <div className="flex items-center gap-2">
-          <span className="h-2 w-2 rounded-full bg-slate-400"></span>
-          <span className="font-medium text-muted-foreground">Pending</span>
-        </div>
+        <OrderStatus status={order.status} />
       </TableCell>
-      <TableCell className="font-medium">Erica Reis</TableCell>
-      <TableCell className="font-medium">$324,00</TableCell>
+      <TableCell className="font-medium">{order.customerName}</TableCell>
+      <TableCell className="font-medium">
+        {order.total.toLocaleString("pt-BR", {
+          style: "currency",
+          currency: "BRL",
+        })}
+      </TableCell>
       <TableCell>
         <Button variant="outline" size="xs">
           <ArrowRight className="mr-2 h-3 w-3" />
-          Approve
+          Aprovar
         </Button>
       </TableCell>
       <TableCell>
         <Button variant="ghost" size="xs">
           <X className="mr-2 h-3 w-3" />
-          Cancel
+          Cancelar
         </Button>
       </TableCell>
     </TableRow>
--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -1,3 +1,8 @@
+import { useQuery } from "@tanstack/react-query";
+import { Helmet } from "react-helmet-async";
+
+import { getOrders } from "@/api/get-orders";
+import { Pagination } from "@/components/pagination";
 import {
   Table,
   TableBody,
@@ -5,18 +10,22 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import { Helmet } from "react-helmet-async";
-import { OrderTableRow } from "./order-table-row";
+
 import { OrderTableFilters } from "./order-table-filters";
-import { Pagination } from "@/components/pagination";
+import { OrderTableRow } from "./order-table-row";
 
 export function Orders() {
+  const { data: result } = useQuery({
+    queryKey: ["orders"],
+    queryFn: getOrders,
+  });
+
   return (
     <>
-      <Helmet title="orders" />
-      <div className="flex flex-col gap-4">
-        <h1 className="text-3xl font-bold tracking-tight">Orders</h1>
+      <Helmet title="Pedidos" />
 
+      <div className="flex flex-col gap-4">
+        <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
         <div className="space-y-2.5">
           <OrderTableFilters />
 
@@ -25,23 +34,24 @@ export function Orders() {
               <TableHeader>
                 <TableRow>
                   <TableHead className="w-[64px]"></TableHead>
-                  <TableHead className="w-[140px]">ID</TableHead>
-                  <TableHead className="w-[180px]">Days since order</TableHead>
+                  <TableHead className="w-[140px]">Identificador</TableHead>
+                  <TableHead className="w-[180px]">Realizado há</TableHead>
                   <TableHead className="w-[140px]">Status</TableHead>
-                  <TableHead>Customer</TableHead>
-                  <TableHead className="w-[140px]">Total</TableHead>
+                  <TableHead>Cliente</TableHead>
+                  <TableHead className="w-[140px]">Total do pedido</TableHead>
                   <TableHead className="w-[164px]"></TableHead>
                   <TableHead className="w-[132px]"></TableHead>
                 </TableRow>
               </TableHeader>
-
               <TableBody>
-                {Array.from({ length: 10 }).map((_, i) => {
-                  return <OrderTableRow key={i} />;
-                })}
+                {result &&
+                  result.orders.map((order) => {
+                    return <OrderTableRow key={order.orderId} order={order} />;
+                  })}
               </TableBody>
             </Table>
           </div>
+
           <Pagination pageIndex={0} totalCount={105} perPage={10} />
         </div>
       </div>
