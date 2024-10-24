727e31c31a87345cae18bcd98f71f1bbff8616fc
EricaReis <ericareis67@outlook.com>
2024-10-23 21:30:11 -0300

    Update order details modal with dynamic info

--- src/pages/app/orders/order-details.tsx
+++ src/pages/app/orders/order-details.tsx
@@ -1,3 +1,5 @@
+import { getOrderDetails } from "@/api/get-order-details";
+import { OrderStatus } from "@/components/order-status";
 import {
   DialogContent,
   DialogDescription,
@@ -13,93 +15,114 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import { useQuery } from "@tanstack/react-query";
+import { formatDistanceToNow } from "date-fns";
 
-export function OrderDetails() {
+export interface OrderDetailsProps {
+  orderId: string;
+  open: boolean;
+}
+
+export function OrderDetails({ orderId, open }: OrderDetailsProps) {
+  const { data: order } = useQuery({
+    queryKey: ["order", orderId],
+    queryFn: () => getOrderDetails({ orderId }),
+    enabled: open,
+  });
+
   return (
     <DialogContent>
       <DialogHeader>
-        <DialogTitle>Order: 4rye3437fuis</DialogTitle>
+        <DialogTitle>Order: {orderId}</DialogTitle>
         <DialogDescription>Order details</DialogDescription>
       </DialogHeader>
 
-      <div className="space-y-6">
-        <Table>
-          <TableBody>
-            <TableRow>
-              <TableCell className="text-muted-foreground">Status</TableCell>
-              <TableCell className="flex justify-end">
-                <div className="flex items-center gap-2">
-                  <span className="h-2 w-2 rounded-full bg-slate-400"></span>
-                  <span className="font-medium text-muted-foreground">
-                    Pending
-                  </span>
-                </div>
+      {order && (
+        <div className="space-y-6">
+          <Table>
+            <TableBody>
+              <TableRow>
+                <TableCell className="text-muted-foreground">Status</TableCell>
+                <TableCell className="flex justify-end">
+                  <OrderStatus status={order.status} />
+                </TableCell>
+              </TableRow>
+
+              <TableRow>
+                <TableCell className="text-muted-foreground">
+                  Customer
+                </TableCell>
+                <TableCell className="flex justify-end">
+                  {order.customer.name}
+                </TableCell>
+              </TableRow>
+
+              <TableRow>
+                <TableCell className="text-muted-foreground">Phone</TableCell>
+                <TableCell className="flex justify-end">
+                  {order.customer.phone ?? "No phone"}
+                </TableCell>
+              </TableRow>
+
+              <TableRow>
+                <TableCell className="text-muted-foreground">E-mail</TableCell>
+                <TableCell className="flex justify-end">
+                  {order.customer.email}
+                </TableCell>
+              </TableRow>
+
+              <TableRow>
+                <TableCell className="text-muted-foreground">
+                  Time since order
+                </TableCell>
+                <TableCell className="flex justify-end">
+                  {formatDistanceToNow(order.createdAt, {
+                    addSuffix: true,
+                  })}
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>Product</TableHead>
+                <TableHead className="text-right">Quantity</TableHead>
+                <TableHead className="text-right">Price</TableHead>
+                <TableHead className="text-right">Subtotal Amount</TableHead>
+              </TableRow>
+            </TableHeader>
+
+            <TableBody>
+              {order.orderItems.map((item) => {
+                return (
+                  <TableRow key={item.id}>
+                    <TableCell>{item.product.name}</TableCell>
+                    <TableCell className="text-right">
+                      {item.quantity}
+                    </TableCell>
+                    <TableCell className="text-right">
+                      {item.priceInCents}
+                    </TableCell>
+                    <TableCell className="text-right">
+                      {(Number(item.priceInCents) / 100) *
+                        (Number(item.quantity) / 100)}
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+
+            <TableFooter>
+              <TableCell colSpan={3}>Order Total</TableCell>
+              <TableCell className="text-right font-medium">
+                {order.totalInCents / 100}
               </TableCell>
-            </TableRow>
-
-            <TableRow>
-              <TableCell className="text-muted-foreground">Customer</TableCell>
-              <TableCell className="flex justify-end">Erica Reis</TableCell>
-            </TableRow>
-
-            <TableRow>
-              <TableCell className="text-muted-foreground">Phone</TableCell>
-              <TableCell className="flex justify-end">92322390123</TableCell>
-            </TableRow>
-
-            <TableRow>
-              <TableCell className="text-muted-foreground">E-mail</TableCell>
-              <TableCell className="flex justify-end">erica@gmailcom</TableCell>
-            </TableRow>
-
-            <TableRow>
-              <TableCell className="text-muted-foreground">
-                Time since order
-              </TableCell>
-              <TableCell className="flex justify-end">5 minutes</TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead>Product</TableHead>
-              <TableHead className="text-right">Quantity</TableHead>
-              <TableHead className="text-right">Price</TableHead>
-              <TableHead className="text-right">Subtotal Amount</TableHead>
-            </TableRow>
-          </TableHeader>
-
-          <TableBody>
-            <TableRow>
-              <TableCell>Corn dog family size</TableCell>
-              <TableCell className="text-right">2</TableCell>
-              <TableCell className="text-right">$5</TableCell>
-              <TableCell className="text-right">$10</TableCell>
-            </TableRow>
-
-            <TableRow>
-              <TableCell>Sunny side up egg</TableCell>
-              <TableCell className="text-right">2</TableCell>
-              <TableCell className="text-right">$3</TableCell>
-              <TableCell className="text-right">$6</TableCell>
-            </TableRow>
-
-            <TableRow>
-              <TableCell>Bagel</TableCell>
-              <TableCell className="text-right">3</TableCell>
-              <TableCell className="text-right">$3</TableCell>
-              <TableCell className="text-right">$9</TableCell>
-            </TableRow>
-          </TableBody>
-
-          <TableFooter>
-            <TableCell colSpan={3}>Order Total</TableCell>
-            <TableCell className="text-right font-medium">$25</TableCell>
-          </TableFooter>
-        </Table>
-      </div>
+            </TableFooter>
+          </Table>
+        </div>
+      )}
     </DialogContent>
   );
 }
